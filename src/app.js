00a8b6f8abc90c0d1529ac34511355f48d471260
Abdullah khan <khanabdullahdln86@gmail.com>
2026-02-09 23:55:29 +0530

    add environment variables to .envsample and implement Cloudinary upload functionality

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
+import cookieParser from "cookie-parser"
 import express from "express"
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(cookieParser())
+app.use(express.json())
 
 export {app}
